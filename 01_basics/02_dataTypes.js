8a078bebf3097ecf4ba0285b77c67f289e3f4866
Anshuman Kumar Yadav <anshukryadav44@gmail.com>
2025-07-29 18:49:11 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all JS code as newer version
+
+// alert( 3 + 3)  // we are usiing nodejs, not browser
+
+console.log(3 + 3)  // code redability should be high
+
+console.log("Anshuman")
+
+let name = "Anshuman"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+// bigint
+// string=> ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
