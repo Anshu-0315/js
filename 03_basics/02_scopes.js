b07ddf893b151ee4a085c2ccdc86fe7fe204c09b
Anshuman Kumar Yadav <anshukryadav44@gmail.com>
2025-08-02 15:11:22 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,17 +1,58 @@
 //var c = 300;
-let a = 300;
+//let a = 300;
 //let a = 10
 //const b = 20
 //var c = 30
 
-if(true){
-    let a = 40
-    const b = 50
-     console.log("INNER:", a);
-    }
+
+
+//if(true){
+    //let a = 40
+    //const b = 50
+     //console.log("INNER:", a);
+    //}
 
     
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c); 
+
+
+function one() {
+    const username = "Anshu";
+
+    function two() {
+        const website = "youtube";
+        //console.log(username);
+    }
+
+   //two();
+}
+//one();
+
+//if (true){
+    //const username = "Anshu";
+    //if (username === "Anshu") {
+        //const website = " youtube";
+       // console.log(username + website);
+   // }
+   // console.log(website);
+    
+
+//console.log(username); 
+
+//++++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1;
+}
+addone(5)
+
+
+const addTwo = function(num){
+    return num + 2;
+}
+addTwo(5)
