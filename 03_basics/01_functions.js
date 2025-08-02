f1e17487f52e35fc8ee6737a8225e97992a3215b
Anshuman Kumar Yadav <anshukryadav44@gmail.com>
2025-08-02 11:49:06 +0000

    function basics

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,8 +32,37 @@ function loginUserMessage(username = "Sam"){
         console.log("Please enter a username");
         return;
     }
-    returm`${username} just logged in`
+    return `${username} just logged in`
 }
 
 //console.log(loginUserMessage("Anshu"))
-console.log(loginUserMessage());
+//console.log(loginUserMessage("Anshu"));
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500, 2000));
+
+
+const user = {
+    username: "Anshu",
+    prices: 200,
+}
+function handleObject(anyobject){console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 350         
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 500]));
