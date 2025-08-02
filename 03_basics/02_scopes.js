e354d36c3d4e06875f3acd618a29665d08e390e7
Anshuman Kumar Yadav <anshukryadav44@gmail.com>
2025-08-02 12:03:16 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+//var c = 300;
+let a = 300;
+//let a = 10
+//const b = 20
+//var c = 30
+
+if(true){
+    let a = 40
+    const b = 50
+     console.log("INNER:", a);
+    }
+
+    
+
+console.log(a);
+//console.log(b);
+//console.log(c); 
